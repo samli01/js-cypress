fad53172c69b31ed8cfc807a86be887ceff87965
Mohamad Samim Ali <65440282+samli01@users.noreply.github.com>
2024-11-24 17:25:41 +0000

    data type

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,16 @@
+let name = "samim"
+let age = 24
+let isPresent = false
+
+/*
+    Number
+    bigint
+    string 
+    boolean
+    null 
+    undifine
+    symbol => Unique
+*/
+
+console.log(typeof null) // the type of null in js is object 
+console.log(typeof undefined )
