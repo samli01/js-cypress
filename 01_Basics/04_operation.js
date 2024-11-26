1b7066c8af6a4e6793cff4498cf8cabdc6379015
Mohamad Samim Ali <65440282+samli01@users.noreply.github.com>
2024-11-26 18:00:58 +0000

    operation

---
+++ 01_Basics/04_operation.js
@@ -0,0 +1,25 @@
+let value = 3
+let negValue = - value // negValue vitra value ko value negative vayera assign hunxa 
+// console.log(negValue);
+
+// console.log(2+2); //add
+// console.log(2-2);    //sub
+// console.log(2*2);    //mul
+// console.log(2/2);    //div
+// console.log(2**3);   //to the power
+// console.log(3%2);    //Remainder
+
+let str1 = "hello "
+let str2 = "samim"
+let str3 = str1 + str2
+console.log(str3); // simple 
+
+console.log(1 + "2"); // yesle 12 print garxa 
+console.log("2" + 1);   //yesle 21 print garxa
+console.log("1" + 2 + 2); //yesle 122 print garxa, kina ki suru ma string xa 
+console.log(1 + 2 + "2");// yesle 32 print garxa, kina ki suru ma string xaina so paile addition operation preform garyo ani tes paxi string lai joddiyo.
+
+console.log(+true);
+
+
+
