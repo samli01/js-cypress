8a323ac6c376d0e1b2b43f93325167b8ca4b7efa
Mohamad Samim Ali <65440282+samli01@users.noreply.github.com>
2024-11-26 18:38:18 +0000

    comparision

---
+++ 01_Basics/05_comparision.js
@@ -0,0 +1,23 @@
+// Basic comparision
+
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// comparision of different datatypes 
+
+//console.log("3" > 2); // yesma true print garxa, same tala ko jasto  
+//console.log("03" > 2); // yesma true print garxa, js le paila string lai number ma convert garxa ni campare garxa 
+
+console.log(null > 0); //false
+console.log(null == 0); // false    tara equality validate garda kheri null matlab empty
+console.log(null >= 0); // true     js ma compare garda kheri null ko value 0 hunxa 
+
+// Strict comparision "===", yesma datatype pani compare hunxa 
+
+console.log("2"===2); // yo false aauxa kina vane yaha data types pani compare gareko xa ra string and number ko comparision nai hudain. 
+
+
+
