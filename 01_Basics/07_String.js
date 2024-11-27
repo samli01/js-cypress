3582984087ce04ac3842443e82268a1e30caef5a
Mohamad Samim Ali <65440282+samli01@users.noreply.github.com>
2024-11-27 17:46:44 +0000

    String and its functions

--- 01_Basics/07_String.js
+++ 01_Basics/07_String.js
@@ -18,3 +18,16 @@ console.log(sliceName);
 
 const sliceName1 = myName.slice(-4,2)
 console.log(sliceName1);
+
+
+const trimName = "  samim    "
+console.log(trimName.trim()); // yesle spaces hatai dinxa both side bata 
+console.log(trimName.trimStart()); // agadi ko space 
+console.log(trimName.trimEnd());  // paxadi ko spaces 
+
+//replace 
+
+const url = "https://redesigned-space-adventure-9w66v4g6g57h9vxj.github.dev/"
+console.log(url.replace("space","samim")); //space lai samim sanga replace garidinxa 
+
+
