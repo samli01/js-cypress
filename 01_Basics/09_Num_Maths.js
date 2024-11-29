ed49b922e73a63bf6a5f95e46b56195d886fdd81
Mohamad Samim Ali <65440282+samli01@users.noreply.github.com>
2024-11-29 18:51:01 +0000

    again

--- 01_Basics/09_Num_Maths.js
+++ 01_Basics/09_Num_Maths.js
@@ -1,7 +1,33 @@
 const score = 100
-console.log(score); //js auto detect it is a number 
-console.log("Datatype of score is",typeof(score)) 
+// console.log(score); //js auto detect it is a number 
+// console.log("Datatype of score is",typeof(score)) 
 
-const Balance = new Number(400) // yaha data type number nai hunuparxa vanera new obj use garko ho.
-console.log(Balance);
-console.log(Balance.toFixed(2)) // yaha decimal paxi 2 digits matra display hunuparxa vanera fix gareko ho.
+const Balance = new Number(400) // yaha Balance ko value number nai hunxa tara yesko datatype object ho.
+// console.log(Balance);
+
+// console.log(Balance.toFixed(2)) // yaha decimal paxi 2 digits matra display hunuparxa vanera fix gareko ho.
+
+// console.log(Balance.toString()); // yaha Balance is converted into String
+// console.log("The length of the Balance is:",Balance.toString(). length);
+
+// console.log(typeof(Balance));
+
+// Balance = 200 //we cannot update the value of constant.
+// console.log(Balance);
+
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-NP')); //yaha hundreds ma vako value lai localeStrig ma display gardxa comma seperated form ma 
+
+// *******************************************************************************************************************************
+
+
+
+console.log(Math);
+console.log(Math.abs(-4)); //Module jsto kaam garxa yesle, Negative lai pani positive banai dinx
+
+
+console.log(Math.random()); // yesma random value 0-1 ko bich ma aauxa.
+
+
+
