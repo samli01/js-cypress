5f77f55a83f04f29fe23f475b9d9d4e1d8df4550
Mohamad Samim Ali <65440282+samli01@users.noreply.github.com>
2024-11-29 18:12:17 +0000

    numbers and Maths in js

---
+++ 01_Basics/09_Num_Maths.js
@@ -0,0 +1,7 @@
+const score = 100
+console.log(score); //js auto detect it is a number 
+console.log("Datatype of score is",typeof(score)) 
+
+const Balance = new Number(400) // yaha data type number nai hunuparxa vanera new obj use garko ho.
+console.log(Balance);
+console.log(Balance.toFixed(2)) // yaha decimal paxi 2 digits matra display hunuparxa vanera fix gareko ho.
