f03c2fdb261982eaecfd9515e1e117ef5ed46e5e
Mohamad Samim Ali <65440282+samli01@users.noreply.github.com>
2024-12-03 17:49:27 +0000

    slice and splicing

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -7,3 +7,23 @@ console.log(brothers[0]);
 const arr2 = new Array(1,2,3,4,5)
 console.log(arr2[2]);
 
+const newArr = myArray.join() //changes into string 
+console.log(newArr);
+
+// Slice and splice
+
+console.log("A ", myArray);
+const arr1 =  myArray.slice(1,3)
+console.log("This is Slice array: ", arr1);
+
+
+console.log("B ", myArray );
+const arr3 = myArray.splice(1,3)
+console.log("This is splice array",arr3);
+console.log("This is after splicing ",myArray);
+
+
+
+
+
+
